16e17768a4a26a2978e00461b22bee24f7de1087
hackerhgl <hackerhgl@gmail.com>
2021-07-30 11:26:18 +0500

    park

---
+++ src/configs/theme.ts
@@ -0,0 +1,6 @@
+export const APP_THEME = {
+  colors: {
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { ThemeProvider, StyleReset } from 'atomize';
-import AppRouter from 'Router';
 
-const theme = {
-  colors: {
-    primary: 'tomato',
-    accent: 'yellow',
-  },
-};
+import { APP_THEME } from 'configs/theme';
+import AppRouter from 'Router';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <StyleReset />
-      {/* <App /> */}
-      <AppRouter />
+    <ThemeProvider theme={APP_THEME}>
+      <StyleReset /> <AppRouter />
     </ThemeProvider>
   );
 }
-
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
