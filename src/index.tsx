8e8325b50dc08ac0b4437191158d755ceab61d79
hackerhgl <hackerhgl@gmail.com>
2021-07-30 10:57:45 +0500

    Init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ThemeProvider, StyleReset } from 'atomize';
+
+const theme = {
+  colors: {
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <StyleReset />
+      {/* <App /> */}
+      <h1>Hello</h1>
+    </ThemeProvider>
+  );
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +32,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
