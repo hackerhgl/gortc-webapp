ef20552cb7d6c8bee38e3c9565fb7b3b4c2ea9da
hackerhgl <hackerhgl@gmail.com>
2021-07-30 16:27:16 +0500

    Links bar

--- src/molecules/Header/index.tsx
+++ src/molecules/Header/index.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { Button, ButtonGroup, Container, Flex, Spacer, Text } from '@chakra-ui/react';
 
 import AppLogo from 'atoms/Logo';
+import { SPACE } from 'configs/space';
+
 import { links } from './data';
 
 function AppHeader(): JSX.Element {
+  const isLoggedIn = false;
   return (
-    <Container shadow="md" maxW="full" padding="6">
+    <Container shadow="md" maxW="full" padding={SPACE[3]}>
       <Flex>
         <AppLogo />
         <Spacer />
-        <ButtonGroup spacing={4} variant="ghost" colorScheme="blue">
-          {links.map((v) => (
-            <Button key={v.label}>
-              <Text key={v.label}>{v.label}</Text>
-            </Button>
-          ))}
+        <ButtonGroup spacing={SPACE[2]} variant="ghost" colorScheme="blue">
+          {links
+            .filter((link) => link.logIn === undefined || isLoggedIn === link.logIn)
+            .map((v) => (
+              <Button key={v.label}>
+                <Text fontSize="sm">{v.label}</Text>
+              </Button>
+            ))}
         </ButtonGroup>
       </Flex>
     </Container>
